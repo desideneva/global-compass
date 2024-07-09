7f3ada973372d7bec955bcd087d34c17154597f3
Desislava Deneva <tolteki3@abv.bg>
2024-07-09 18:46:17 +0000

    Modify actions page styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,8 @@
+ 
 
-function showSignUp() {
-    document.querySelector('.btn-container').style.display = 'none';
-    document.getElementById('signUpContainer').style.display = 'flex';
-    document.getElementById('signInContainer').style.display = 'none';
-    document.getElementById('shareStoryContainer').style.display = 'none';
-}
 
-function showSignIn() {
-    document.querySelector('.btn-container').style.display = 'none';
-    document.getElementById('signInContainer').style.display = 'flex';
-    document.getElementById('signUpContainer').style.display = 'none';
-    document.getElementById('shareStoryContainer').style.display = 'none';
-}
+ 
 
-function showShareStory() {
-    document.querySelector('.btn-container').style.display = 'none';
-    document.getElementById('shareStoryContainer').style.display = 'flex';
-    document.getElementById('signUpContainer').style.display = 'none';
-    document.getElementById('signInContainer').style.display = 'none';
-}
+
+
+

20850132975b2f98246736a0140d385d8fe0ed40
Desislava Deneva <tolteki3@abv.bg>
2024-07-11 11:53:01 +0000

    Add templates folder

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask('__name__')
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template("index.html")
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
 if __name__ == '__main__':
     app.run(debug=True)
-    
+
 
 
 
